cd9f976624c5acf8bf1d8d8905074675724246bf
ryanvling <ryanvling@gmail.com>
2025-06-24 16:12:23 +1000

    initial commit

---
+++ calculator.c
@@ -0,0 +1,18 @@
+// Calculator
+//
+// Perform basic arithmetic operations.
+//
+// Author: Ryan Ling (ryanvling@gmail.com)
+//
+// Written: 24/6/2025
+
+#include <stdio.h>
+
+int main() {
+    int result = 0;
+
+    printf("Enter operation:\n> ");
+
+    printf("%d", result);
+    return 0;
+}
